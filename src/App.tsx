3bd5e2079dd0cdb78fce62dfaab295648ecc1ef2
naoyuki-hasegawa <naoyuki.hasegawa@freeeks.jp>
2024-04-13 19:29:14 +0900

    Add rtk-test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useRef } from "react";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+
+import { Todo, todoApi } from "./store";
+
+function TodoApp() {
+  const { data: todos } = todoApi.useGetAllQuery();
+  const [updateTodo] = todoApi.useUpdateTodoMutation();
+  const [deleteTodo] = todoApi.useDeleteTodoMutation();
+  const [addTodo] = todoApi.useAddTodoMutation();
+
+  const onToggle = useCallback(
+    (todo: Todo) => updateTodo({ ...todo, done: !todo.done }),
+    [updateTodo]
+  );
+
+  const onDelete = useCallback((todo: Todo) => deleteTodo(todo), [deleteTodo]);
+
+  const textRef = useRef<HTMLInputElement>(null);
+
+  const onAdd = useCallback(
+    () => addTodo(textRef.current!.value ?? ""),
+    [addTodo]
+  );
+
+  return (
+    <div className="App">
+      <div className="todos">
+        {todos?.map((todo) => (
+          <React.Fragment key={todo.id}>
+            <div>
+              <input
+                type="checkbox"
+                checked={todo.done}
+                onChange={() => onToggle(todo)}
+              />
+              <span>{todo.text}</span>
+            </div>
+            <button onClick={() => onDelete(todo)}>Delete</button>
+          </React.Fragment>
+        ))}
+      </div>
+      <div className="add">
+        <input type="text" ref={textRef} />
+        <button onClick={onAdd}>Add</button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApiProvider api={todoApi}>
+      <TodoApp />
+    </ApiProvider>
   );
 }
 
